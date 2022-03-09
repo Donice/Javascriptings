c563c6e3512e028c473a8045412bceb710ca8e79
Donice Ubaru <87942465+Donice@users.noreply.github.com>
2022-03-09 13:19:08 +0100

    Add files via upload

---
+++ videQuestios.js
@@ -0,0 +1,27 @@
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is " 
+
+// create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+let name = "Donice";
+let greetings = "Hi, my name is";
+let myGreeting = greetings + " " + name;
+
+// console.log(myGreeting); 
+
+// let points = 4
+// let bonusPoints = "10"
+
+// let totalPoints   = points + bonusPoints;
+// console.log(totalPoints)
+
+console.log(4 + 5)
+console.log("2" + "4")
+console.log("5" + 1)
+console.log(100 + "100") 
+
